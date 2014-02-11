a3d2f12b3e647b3589d5c6bdae966758d5bcfb7e
Devon Tivona <devon@tivona.me>
2014-02-10 18:57:15 -0700

    Modify task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,28 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def new
-    @tasks = Task.new
-  end
-
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url, notice: "Analyst #{@task} was successfully created." }
+        format.json { render action: 'show', status: :created, location: @task }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def dataset_params
+    params.require(:task).permit(:title, :location, :reward, :due_date)
   end
 end
